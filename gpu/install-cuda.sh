d01f126ab475283e52c40aacb6d5cc663d6da677
Marco De La Pierre <marco.delapierre@gmail.com>
2022-11-29 22:17:22 +0800

    minor edit in gpu/cuda

--- gpu/install-cuda.sh
+++ gpu/install-cuda.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-# note: requires sudo powers
-
 # from 
 # https://developer.nvidia.com/cuda-downloads?target_os=Linux&target_arch=x86_64&target_distro=Ubuntu&target_version=1804&target_type=deblocal
 
