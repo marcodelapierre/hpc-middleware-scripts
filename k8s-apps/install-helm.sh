f89438e78777aa6b04f6bf523affd55963744448
Dr Marco De La Pierre <marco.delapierre@gmail.com>
2026-06-19 16:48:27 +0800

    k8s-apps: small script updates
    Signed-off-by: Dr Marco De La Pierre <marco.delapierre@gmail.com>

--- k8s-apps/install-helm.sh
+++ k8s-apps/install-helm.sh
@@ -5,6 +5,6 @@ HELM_VERSION="4.1.1"
 wget https://get.helm.sh/helm-v${HELM_VERSION}-linux-amd64.tar.gz
 tar xzf helm-v${HELM_VERSION}-linux-amd64.tar.gz
 
-sudo mv linux-amd64/helm /usr/local/bin/
+sudo install linux-amd64/helm /usr/local/bin/
 
 rm -r helm-v${HELM_VERSION}-linux-amd64.tar.gz linux-amd64/
--- k8s-apps/install-kubectl.sh
+++ k8s-apps/install-kubectl.sh
@@ -2,5 +2,6 @@
 
 kubectl_ver="v1.35.0"
 
-sudo curl -L "https://dl.k8s.io/release/${kubectl_ver}/bin/linux/amd64/kubectl" -o /usr/local/bin/kubectl
-sudo chmod +x /usr/local/bin/kubectl
+curl -L "https://dl.k8s.io/release/${kubectl_ver}/bin/linux/amd64/kubectl"
+sudo install kubectl /usr/local/bin/
+rm kubectl
